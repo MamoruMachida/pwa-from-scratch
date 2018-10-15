8340e756970a6bb3f5d016f61ecfbdc28013c2f4
MamoruMachida <mamoru.machida@fox-hound.jp>
2018-10-15 18:55:08 +0900

    support offline

--- sw.js
+++ sw.js
@@ -2,6 +2,8 @@ const staticAssets = [
     './',
     './style.css',
     './app.js',
+    './fallback.json',
+    './images/fetch-dog.jpg'
 ]
 
 self.addEventListener('install', async () => {
@@ -10,12 +12,28 @@ self.addEventListener('install', async () => {
 });
 
 self.addEventListener('fetch', e => {
-    console.log(`fetch`);
     const req = e.request;
-    e.respondWith(cacheFirst(req));
+    const url = new URL(req.url);
+    if (url.origin === location.origin) {
+        e.respondWith(cacheFirst(req));
+    } else {
+        e.respondWith(networkFirst(req));
+    }
 });
 
 async function cacheFirst(req) {
-    const cachedResposnse = await caches.match(req);
-    return cachedResposnse || fetch(req);
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+    const cache = await caches.open('news-dynamic');
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        const cachedResponse = await cache.match(req);
+        return cachedResponse || await caches.match('./fallback.json');
+    }
 }
