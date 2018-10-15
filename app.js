8f65cd1fae89f7aff09ab03603cdd0824257f445
MamoruMachida <mamoru.machida@fox-hound.jp>
2018-10-15 18:10:39 +0900

    nopwa

---
+++ app.js
@@ -0,0 +1,42 @@
+const apiKey = '87fe463ccbef4c9cabc36aad233d2fc6';
+const main = document.querySelector('main');
+const sourceSelector = document.querySelector('#sources');
+const defaultSource = 'the-washington-post';
+
+window.addEventListener('load', async e => {
+    updateNews();
+    await updateSources();
+    sourceSelector.value = defaultSource;
+
+    sourceSelector.addEventListener('change', e => {
+        updateNews(e.target.value);
+    });
+});
+
+async function updateSources() {
+    const res = await fetch(`http://newsapi.org/v1/sources`);
+    const json = await res.json();
+
+    sourceSelector.innerHTML = json.sources
+        .map(src => `<option value="${src.id}">${src.name}</option>`)
+        .join('\n');
+}
+
+async function updateNews(source = defaultSource) {
+    const res = await fetch(`http://newsapi.org/v1/articles?source=${source}&apiKey=${apiKey}`)
+    const json = await res.json();
+
+    main.innerHTML = json.articles.map(createArticle).join('\n');
+}
+
+function createArticle(article) {
+    return `
+        <div class="article">
+            <a href="${article.url}">
+                <h2>${article.title}</h2>
+                <img src="${article.urlToImage}" alt="${article.title}">
+                <p>${article.description}</p>
+            </a>
+        </div>
+    `;
+}
