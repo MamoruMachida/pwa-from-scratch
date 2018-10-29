02a0b0866e3befc53deb244310dcc62ece4b37ff
MamoruMachida <mamoru.machida@fox-hound.jp>
2018-10-29 18:21:36 +0900

    create subscribe button and add effects

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+/* eslint-env browser, es7 */
+'use strict';
+
 const apiKey = '87fe463ccbef4c9cabc36aad233d2fc6';
+const applicationServerPubKey = 'BLEp9zk0_wpydHwcyX1r99rd6pJiQXsB_f4WU-qo_Y_XCoIQ8DRS0ROYncracaUXV7OZHmrT3OehSVX8LnfkI0M';
 const main = document.querySelector('main');
 const sourceSelector = document.querySelector('#sources');
 const defaultSource = 'the-washington-post';
+const subscribeButton = document.querySelector('#subscribeButton');
+let isSubscribed;
+let swRegistration;
 
 window.addEventListener('load', async e => {
     updateNews();
@@ -16,13 +23,58 @@ window.addEventListener('load', async e => {
     if('serviceWorker' in navigator) {
         try {
             // sw.jsをサービスワーカーに登録
-            navigator.serviceWorker.register('sw.js');
+            swRegistration = await navigator.serviceWorker.register('sw.js');
             console.log(`SW registered`);
         } catch (error) {
             console.log(`SW not registered`);
         }
+
+        // PushManagerに対応しているか確認する
+        if ('PushManager' in window) {
+            console.log('Push is supported');
+            initializeSubscribeBtn();
+        } else {
+            console.warn('Push messaging is not supported');
+            subscribeButton.textContent = 'Push Not Supported';
+            subscribeButton.disabled = true;
+        }
     }
 });
+
+async function initializeSubscribeBtn() {
+
+    subscribeButton.addEventListener('click', () => {
+        subscribeButton.disabled = true;
+        if (isSubscribed) {
+            // TODO: un subscribe user
+        } else {
+            subscribeUser();
+        }
+    });
+
+    // Set the initial subscription value
+    const subscription = await swRegistration.pushManager.getSubscription();
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+        console.log('User is subscribed.');
+    } else {
+        console.log('User is NOT subscribed.');
+    }
+    updateSubscribeBtn();
+}
+
+function updateSubscribeBtn() {
+    if (Notification.permission === 'denied') {
+        subscribeButton.textContent = 'Push Messaging Blocked.';
+        subscribeButton.disabled = true;
+        updateSubscriptionOnServer(null);
+        return;
+    }
+
+    subscribeButton.textContent = isSubscribed ? 'Un Subscribe' : 'Subscribe';
+    subscribeButton.disabled = false;
+}
 
 async function updateSources() {
     const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${apiKey}`);
@@ -31,6 +83,68 @@ async function updateSources() {
     sourceSelector.innerHTML = json.sources
         .map(src => `<option value="${src.id}">${src.name}</option>`)
         .join('\n');
+}
+
+function subscribeUser() {
+    const applicationServerKey = urlB64ToUint8Array(applicationServerPubKey);
+    swRegistration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: applicationServerKey
+    })
+    .then((subscription) => {
+        console.log('User is subscribed');
+        console.log(JSON.stringify(subscription));
+
+        updateSubscriptionOnServer(subscription);
+
+        isSubscribed = true;
+
+        updateSubscribeBtn();
+    })
+    .catch((err) => {
+        console.log(`Failed to subscribe the user: ${err}`);
+        updateSubscribeBtn();
+    });
+}
+
+function unsubscribeUser() {
+    swRegistration.pushManager.getSubscription()
+    .then(function(subscription) {
+      if (subscription) {
+        return subscription.unsubscribe();
+      }
+    })
+    .catch(function(error) {
+      console.log('Error unsubscribing', error);
+    })
+    .then(function() {
+      updateSubscriptionOnServer(null);
+
+      console.log('User is unsubscribed.');
+      isSubscribed = false;
+
+      updateSubscribeBtn();
+    });
+  }
+
+function updateSubscriptionOnServer(subscription) {
+     // TODO: Send subscription to application server
+    console.log(subscription);
+}
+
+function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
 }
 
 async function updateNews(source = defaultSource) {
--- sw.js
+++ sw.js
@@ -1,3 +1,27 @@
+/* eslint-env browser, serviceworker, es6 */
+'use strict';
+
+/* eslint-disable max-len */
+
+const applicationServerPublicKey = 'BH8-hIchXKMI6AKSee8gD0hhPThRqaEhIEtMJwcTjEQhiOKdG-_2tTIO-6hOAK4kwg5M9Saedjxp4hVE-khhWxY';
+
+/* eslint-enable max-len */
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 // CacheStorageに登録する名前
 const cacheName = 'news-v1';
 // キャッシュしておきたいstaticAssets
@@ -6,8 +30,11 @@ const staticAssets = [
     './style.css',
     './app.js',
     './fallback.json',
-    './images/fetch-dog.jpg'
+    './images/fetch-dog.jpg',
+    './images/icon.png',
+    './images/badge.png'
 ];
+const applicationServerPrivKey = 'e61JIYf95H0UjWQHXZF99-HDxVF3q3OBc5YLyz0dUC8';
 
 // ServiceWorkerへの登録が済んだ際に発行される'install'イベントへの紐付け
 self.addEventListener('install', async () => {
@@ -19,7 +46,7 @@ self.addEventListener('install', async () => {
 });
 
 // fetch()が呼ばれた際のイベントへの紐付けを行います。
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', async (e) => {
     const req = e.request;
     const url = new URL(req.url);
     // アクセスが自身のページか、外部ページかどうか判定しています。
@@ -39,16 +66,42 @@ async function cacheFirst(req) {
     return cachedResponse || fetch(req);
 }
 
-// リクエストのレスポンス>キャッシュ>エラーデータの順で取得する。
+// リクエストのレスポンス>キャッシュ>エラーデータの順で取得する。
 // 今回は外部ページへのリクエストの制御に使用しています。
 async function networkFirst(req) {
     const cache = await caches.open('news-dynamic');
     try {
         const res = await fetch(req);
-        cache.put(req, res.clone());
+        await cache.put(req, res.clone());
         return res;
     } catch (error) {
         const cachedResponse = await cache.match(req);
         return cachedResponse || await caches.match('./fallback.json');
     }
 }
+
+// pushイベントを感知してNotificationを表示する
+self.addEventListener('push', function (event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+    const title = 'Push Codelab';
+    const options = {
+        body: 'Yay it works.',
+        icon: './images/icon.png',
+        badge: './images/badge.png'
+    };
+
+    // waitUntilはPromiseを受け取り、そのPromiseが解決されるまで処理を継続する。
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', (event) => {
+    console.log('[Service Worker] Notification click Received.');
+
+    event.notification.close();
+
+    event.waitUntil(
+        clients.openWindow('https://developers.google.com/web/')
+    );
+});
